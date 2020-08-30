d301bee074be1cf0f572f2dd41442caf98da3379
Hritiksum <hritik.sum@gmail.com>
2020-08-30 12:49:35 +0530

    models updated

--- theatre/api/models.py
+++ theatre/api/models.py
@@ -24,3 +24,6 @@ class Ticket_detail(models.Model):
     customer=models.ForeignKey(Customer,on_delete=models.CASCADE)
     movie_detail=models.ForeignKey(Movie_detail,on_delete=models.CASCADE)
     tickets_quantity=models.IntegerField(default=1, validators=[MaxValueValidator(20), MinValueValidator(1)])
+
+    def __str__(self):
+        return "%s {%s} (%s)" % (self.customer.name,self.movie_detail.movie_name , self.tickets_quantity)
